a0f586cdffaa44ecadea377eaa4ecb831e011cf7
djosix <djosicks@gmail.com>
2022-10-08 00:39:01 +0800

    add chordprog demo

---
+++ static/chordprog/precache-manifest.cd6328dc2f4a41e23852c9ea1031044b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e45b97e621c072a71c",
+    "url": "css/app.69c8c5c1.css"
+  },
+  {
+    "revision": "8d8f6ad2e09bfb37952d4e08360eed38",
+    "url": "index.html"
+  },
+  {
+    "revision": "c5e45b97e621c072a71c",
+    "url": "js/app.88e08d88.js"
+  },
+  {
+    "revision": "a03101b1e95a5929c3d5",
+    "url": "js/chunk-vendors.68147b12.js"
+  },
+  {
+    "revision": "f638758afa38d12c5216ff3f90296616",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
